ea0fa750df440d95aab59403fb579650e7152059
Tyler Hunt <tyler.hunt@utah.edu>
2016-10-28 13:45:01 -0600

    initialize package

---
+++ R/books.R
@@ -0,0 +1,16 @@
+#' @title books
+#'
+#' @importFrom httr GET
+#' @export
+
+books <- function(bookNum = NULL, name = NULL, fromReleaseDate = NULL, toReleaseDate = NULL, page = NULL, pageSize = NULL){
+
+  if(all(is.null(bookNum) & is.null(name) & is.null(fromReleaseDate) & is.null(toReleaseDate) & is.null(page) & is.null(pageSize))){
+    url <- 'http://www.anapioficeandfire.com/api/books'
+  }
+
+  Cont <- GET(url)
+
+  fromJSON(httr::content(Cont, 'text'))
+
+}
